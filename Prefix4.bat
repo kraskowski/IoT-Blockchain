2dc5f56e3c1cf9d12e3484593d54daf914a369ea
Florian Kraskowski <84330084+kraskowski@users.noreply.github.com>
2023-03-01 14:24:25 +0100

    Create Prefix4.bat

---
+++ Prefix4.bat
@@ -0,0 +1,37 @@
+@echo off
+setlocal EnableDelayedExpansion
+
+REM Prompt the user to select files
+set "files="
+echo Please select files to rename:
+echo.
+set "vbsfile=%temp%\_.vbs"
+>"%vbsfile%" echo set WshShell=WScript.CreateObject("WScript.Shell") 
+>>"%vbsfile%" echo set oExec=WshShell.Exec("mshta.exe ""about:<input type=file name=FILE accept='*.*' multiple=true onchange=\\'setInterval(function(){if(FILE.value){parent.document.title=\'done\';clearInterval();}},250);\\' id=FILE><script>FILE.click();new ActiveXObject('Scripting.FileSystemObject').GetStandardStream(1).WriteLine(FILE.value);</script>""") 
+>>"%vbsfile%" echo Do While oExec.Status = 0 : WScript.Sleep 100 : Loop 
+>>"%vbsfile%" echo WScript.StdOut.WriteLine oExec.StdOut.ReadAll 
+set "filelist=%temp%\_.filelist"
+for /f "delims=" %%f in ('cscript //nologo "%vbsfile%"') do (
+    set "file=%%~f"
+    if defined file (
+        set "files=!files! "%%~f""
+        echo - "%%~f"
+    )
+)
+if not defined files (
+    echo No files selected.
+    goto :eof
+)
+
+REM Prompt the user to enter the prefix
+set /p "prefix=Enter a prefix: "
+
+REM Rename the files with the prefix
+for %%f in (%files%) do (
+    set "filename=%%~nxf"
+    ren "%%~f" "%prefix%!filename!"
+)
+
+echo Files renamed with prefix "%prefix%".
+
+pause
