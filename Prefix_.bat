f1349b3dff02a8b193885faac881280e5d2654b9
Florian Kraskowski <84330084+kraskowski@users.noreply.github.com>
2023-03-01 13:44:35 +0100

    Update Prefix_.bat

--- Prefix_.bat
+++ Prefix_.bat
@@ -1,14 +1,27 @@
 @echo off
-set /p prefix=Enter prefix:
-powershell.exe -Command "& {$f=Get-ChildItem -Path . -Filter *.txt -File | Select-Object -ExpandProperty FullName; $f -join [Environment]::NewLine}" > files.txt
 setlocal EnableDelayedExpansion
-set files=
-for /f "delims=" %%f in (files.txt) do set files=!files! "%%f"
+
+REM Use PowerShell to prompt user to select files
+echo Please select the files to rename:
+set "files="
+for /f "delims=" %%f in ('powershell.exe -Command "Add-Type -AssemblyName System.Windows.Forms; $ofd = New-Object System.Windows.Forms.OpenFileDialog; $ofd.Multiselect = $true; $ofd.Filter = 'All files (*.*)|*.*'; $null = $ofd.ShowDialog(); $ofd.FileNames"') do (
+  set "files=!files! "%%f""
+)
+if not defined files (
+  echo No files selected.
+  goto end
+)
+
+REM Prompt user for prefix
+set /p prefix=Enter prefix:
+
+REM Rename files with prefix
 for %%f in (%files%) do (
   set "name=%%~nxf"
   set "dir=%%~dpf"
   ren "%%f" "%prefix%!name!"
 )
-del files.txt
+
 echo Done!
+: end
 pause
